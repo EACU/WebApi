4a815e354aef2957fa13b6487da2955ec77b3a6a
eqip3u <eqip3u.dem@yandex.ru>
2018-11-10 21:08:01 +0500

    IC - user get roles in response

--- Controllers/Account/InformationController.cs
+++ Controllers/Account/InformationController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using EACA_API.Data;
+using Microsoft.AspNetCore.Identity;
+using EACA_API.Models.Account;
 
 namespace EACA_API.Controllers.Account
 {
@@ -14,11 +16,13 @@ namespace EACA_API.Controllers.Account
     public class InformationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApiUser> _userManager;
         private readonly ClaimsPrincipal _caller;
 
-        public InformationController(ApplicationDbContext appDbContext, IHttpContextAccessor httpContextAccessor)
+        public InformationController(ApplicationDbContext appDbContext, UserManager<ApiUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _context = appDbContext;
+            _userManager = userManager;
             _caller = httpContextAccessor.HttpContext.User;
         }
 
@@ -27,8 +31,9 @@ namespace EACA_API.Controllers.Account
         {
             var userId = _caller.Claims.Single(c => c.Type == ClaimsIdentity.DefaultNameClaimType).Value;
             var user = await _context.Users.SingleAsync(x => x.Id == userId);
+            var roles = await _userManager.GetRolesAsync(user);
 
-            return Ok(new { user.FirstName, user.LastName, user.PictureUrl });
+            return Ok(new { user.FirstName, user.LastName, user.PictureUrl, roles });
         }
     }
 }
