45a472852c98001267dc698ea5b3d9de34e27ee8
eqip3u <eqip3u.dem@yandex.ru>
2018-11-11 13:59:02 +0500

    PUT user information

--- Controllers/Account/InformationController.cs
+++ Controllers/Account/InformationController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using EACA_API.Data;
 using Microsoft.AspNetCore.Identity;
 using EACA_API.Models.Account;
+using EACA_API.ViewModels.Accounts;
 
 namespace EACA_API.Controllers.Account
 {
@@ -29,11 +30,47 @@ namespace EACA_API.Controllers.Account
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var userId = _caller.Claims.Single(c => c.Type == ClaimsIdentity.DefaultNameClaimType).Value;
-            var user = await _context.Users.SingleAsync(x => x.Id == userId);
-            var roles = await _userManager.GetRolesAsync(user);
+            var user = await GetApiUser();
 
-            return Ok(new { user.FirstName, user.LastName, user.PictureUrl, roles });
+            UserInformationViewModel userViewModel = await GenerateUserInformationViewModel(user);
+
+            return Ok(userViewModel);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody]UserInformationViewModel body)
+        {
+            var user = await GetApiUser();
+
+            user.FirstName = body.FirstName;
+            user.LastName = body.LastName;
+            user.PhoneNumber = body.PhoneNumber;
+            user.PictureUrl = body.PictureUrl;
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
+            UserInformationViewModel userViewModel = await GenerateUserInformationViewModel(user);
+
+            return Ok(userViewModel);
+        }
+
+        private async Task<ApiUser> GetApiUser()
+        {
+            var userId = _caller.Claims.Single(c => c.Type == ClaimsIdentity.DefaultNameClaimType).Value;
+            return await _context.Users.SingleAsync(x => x.Id == userId);
+        }
+
+        private async Task<UserInformationViewModel> GenerateUserInformationViewModel(ApiUser user)
+        {
+            return new UserInformationViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                PictureUrl = user.PictureUrl,
+                Roles = await _userManager.GetRolesAsync(user)
+            };
         }
     }
 }
---
+++ ViewModels/Accounts/UserInformationViewModels/UserInformationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EACA_API.ViewModels.Accounts
+{
+    public class UserInformationViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; }
+
+        [Required]
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
